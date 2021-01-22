ca1a5afdcf639d798e6d2572afd90e3ee1c14942
bhardwajankur9 <77745944+bhardwajankur9@users.noreply.github.com>
2021-01-22 14:16:24 +0530

    Update Constants.java

--- src/test/java/com/ankurAPI/utilities/Constants.java
+++ src/test/java/com/ankurAPI/utilities/Constants.java
@@ -3,4 +3,5 @@ package com.ankurAPI.utilities;
 public class Constants {
 	public static final String BASE_URL = "https://petstore.swagger.io/v2/";
 	public static final String GET_AVAILABLE_PET = "pet/findByStatus?status=available";
+	public static final String POST_PET = "pet"
 }
